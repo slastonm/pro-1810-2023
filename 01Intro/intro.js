b951dbe5347d9f1725238093206c27d733cd4f08
slastonm <slastonm@gmail.com>
2023-10-18 21:42:58 +0300

    first commit

---
+++ 01Intro/intro.js
@@ -0,0 +1,68 @@
+// alert('Hello from file intro.js'); Ctrl+/ Cmnd+/
+// Coment text for study
+
+var oldBox = 'apple';
+let newBox = 'orange';
+const USERKEY = 'Secret value';
+
+let a = 3;
+let b = 2;
+
+// заборонено
+// let 12value = 12; цифри на початку 
+// let va lue = 12;  пробіли
+// let my^name = 'Max'; спецсимволи
+// let const = 'value';
+
+
+let myname = 'Tom';
+let MyName = 'Tom2';
+let myName = 'Tom3';
+let MYNAME = 'Tom4';
+
+let stringValue = "My string value";
+let anotherStringValue = 'My string value too';
+
+let firstValue = '10';
+let secondValue = '20';
+let result = firstValue+secondValue;
+let numberResult = a+b;
+console.log(10+stringValue);
+
+let userAge = 25;
+let userDrinkValue = 0.03;
+let nanValue = NaN;
+let infinityValue = Infinity;
+
+
+let fruts = 'orange';
+
+fruts = 'pear';
+
+console.log(fruts);
+document.write(fruts);
+
+// Boolean
+let trueValue = true;
+let falseValue = true;
+
+let userScore = 170;
+let scoreResult = userScore >80;
+console.log(scoreResult);
+
+//Null 
+let noValue = null;
+
+// Undefined 
+let noFind = undefined;
+console.log(a);
+
+// Work with user
+
+// alert('Hello js');
+// alert(anotherStringValue);
+
+let dayInfo = prompt('Як ваш день?');
+let ageInfo= prompt('Напишіть свій вік');
+console.log(dayInfo);
+console.log(ageInfo);
---
+++ 01Intro/numberMath.js
@@ -0,0 +1,29 @@
+// let ageInfo= prompt('Напишіть свій вік');
+// console.log(ageInfo);
+
+// let convertStr = + +ageInfo;
+// let convertStr = parseInt(ageInfo);
+// let convertStr = parseFloat(ageInfo);
+// let convertStr = Number(ageInfo);
+
+
+let productPrice = Number(prompt('Type price'));
+let productAmount =Number(prompt('Type how many products you buy'));
+
+
+console.log(productPrice);
+console.log(productAmount);
+
+
+let summ = 10 + productAmount;
+let minusValue = productAmount - 30;
+let multiply = 20*30;
+let divide = 100/30;
+
+console.log(summ);
+console.log(minusValue);
+console.log(multiply);
+console.log(divide);
+
+
+
